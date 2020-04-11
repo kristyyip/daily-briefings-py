14577f7de92e5436d41efd98c833b3ddc932d175
kristyyip <59596642+kristyyip@users.noreply.github.com>
2020-04-10 21:28:33 -0400

    User registration

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/home_routes.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, flash
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -17,7 +17,18 @@ def about():
     #return "About Me (TODO)"
     return render_template("about.html")
 
-@home_routes.route("/register")
+@home_routes.route("/users/new")
 def register():
-    print("VISITED THE REGISTRATION PAGE")
-    return "Sign Up for our Product! (TODO)"
+    print("VISITED THE NEW USER REGISTRATION PAGE...")
+    #return "Sign Up for our Product! (TODO)"
+    return render_template("registration_form.html")
+
+@home_routes.route("/users/create", methods=["POST"])
+def create_user():
+    print("RECEIVED FORM INPUTS")
+    print("FORM DATA:", dict(request.form)) #> {'full_name': 'Example User', 'email_address': 'me@example.com', 'country': 'US'}
+    user = dict(request.form)
+    # todo: store in a database or google sheet!
+    #flash(f"User '{user['full_name']}' created successfully!", "success")
+    flash(f"User '{user['full_name']}' created successfully! (TODO)", "warning")
+    return redirect("/")
